2d6049717e67f0c18ae8eead4083927a73eca48d
“jramma” <pepe.ramos@mortensen.cat>
2024-01-23 20:29:40 +0100

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Invoicess(){
+    return <p>Invoices</p>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './ui/global.css';
+import {montserrat} from './ui/fonts';
+
 
 export default function RootLayout({
   children,
@@ -7,7 +9,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className='py-10 flex justify-center items-center'>
+          Hecho con ❤️ por Jramma
+        </footer>
+        </body>
     </html>
   );
 }
